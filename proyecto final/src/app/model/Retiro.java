58c6822e3bdf406b24ed20ead0fac96f146a07e7
Simon <simon.velasquezp@uqvirtual.edu.co>
2025-11-02 23:32:33 -0500

    Implement withdrawal logic in Retiro class
    Added aplicarTransaccion method to handle withdrawals.

--- proyecto final/src/app/model/Retiro.java
+++ proyecto final/src/app/model/Retiro.java
@@ -4,5 +4,14 @@ public class Retiro extends Transaccion {
     public Retiro(double monto, LocalDate fecha, String cliente, String descripcion){
         super(monto, fecha, cliente, descripcion);
     }
-    
+     @Override
+    public void aplicarTransaccion(CuentaBase cuenta){
+        if (cuenta.getSaldo() >= getMonto()) {
+            cuenta.setSaldo(cuenta.getSaldo() - getMonto());
+            System.out.println("Retiro de $" + getMonto() + " realizado. Nuevo saldo: $" + cuenta.getSaldo());
+        } else {
+            System.out.println("Fondos insuficientes para realizar el retiro.");
+        }
+    }
 }
+
