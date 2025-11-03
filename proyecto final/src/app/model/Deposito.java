dfcdeda63160d29b1b557cb901804e8ba1c0576b
Simon <simon.velasquezp@uqvirtual.edu.co>
2025-11-02 23:31:31 -0500

    Implement aplicarTransaccion in Deposito class
    Added aplicarTransaccion method to Deposito class

--- proyecto final/src/app/model/Deposito.java
+++ proyecto final/src/app/model/Deposito.java
@@ -4,7 +4,12 @@ public  class Deposito extends Transaccion {
     public Deposito(double monto, LocalDate fecha, String cliente, String descripcion){
         super(monto, fecha, cliente, descripcion);
     }
-
+ @Override
+    public void aplicarTransaccion(CuentaBase cuenta){
+        cuenta.setSaldo(cuenta.getSaldo()+getMonto());
+        System.out.println("Desposito de:"+ getMonto()+ "realizado. Nuevo saldo de:"+ cuenta.getSaldo());
+    }
     
     
 }
+
