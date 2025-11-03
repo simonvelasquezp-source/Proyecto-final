5417b71bc0e2c498404cd4f0c13ed49ba8d0094f
Simon <simon.velasquezp@uqvirtual.edu.co>
2025-11-02 23:22:34 -0500

    Implement Puntos class with properties and methods
    Se actualizo la clase Puntos.

--- proyecto final/src/app/model/Puntos.java
+++ proyecto final/src/app/model/Puntos.java
@@ -1,5 +1,26 @@
 package app.model;
 
 public class Puntos {
-    
+    private Usuario propietario;
+    private int puntos;
+    public Puntos( Usuario propietario){
+        this.propietario=propietario;
+        this.puntos=0;
+    }
+    public Usuario getPropietario(){
+        return propietario;
+    }
+    public void setPropietario(Usuario propietario){
+        this.propietario=propietario;
+    }
+    public int getPuntos(){
+        return puntos;
+    }
+    public void setPuntos(int puntos){
+        this.puntos=puntos;
+    }
+    public void agregarPuntos(int cantidad){
+        this.puntos+= cantidad;
+    }
 }
+
