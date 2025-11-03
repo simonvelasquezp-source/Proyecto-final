8da89a6c039e218cd104418d9076911fd1faea3f
Simon <simon.velasquezp@uqvirtual.edu.co>
2025-11-02 23:39:27 -0500

    Refactor Monedero class with new attributes and methods
    Updated Monedero class to include Puntos and saldo attributes, modified constructor, and added depositar and obtenerRangoCliente methods.

--- proyecto final/src/app/model/Monedero.java
+++ proyecto final/src/app/model/Monedero.java
@@ -1,11 +1,11 @@
 package app.model;
 
 public class Monedero extends CuentaBase{
-    private int puntos;
-    public Monedero(String numeroCuenta, Usuario propietario){
-        super(numeroCuenta, propietario);
-
-        this.puntos=0;
+    private Puntos puntos;
+    public Monedero(String numeroCuenta, Usuario propietario, String id, double saldo){
+        super(numeroCuenta, propietario, id);
+        this.puntos=new Puntos(propietario);
+        this.saldo=saldo;
     }
     public int getPuntos(){
         return puntos;
@@ -18,5 +18,16 @@ public class Monedero extends CuentaBase{
         System.out.println("Monedero de " + propietario.getNombre() + 
                            " | Saldo: " + saldo + " | Puntos: " + puntos);
     }
-    
+    public void depositar(double monto) {
+        saldo += monto;
+       
+        int puntosGanados = (int)(monto / 1000 * 10);
+        puntos.agregarPuntos(puntosGanados);
+    }
+
+    public String obtenerRangoCliente() {
+        int totalPuntos = puntos.getPuntos();
+        return RangoCliente.determinarRango(totalPuntos);
+    }
 }
+
