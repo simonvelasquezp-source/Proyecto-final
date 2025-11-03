24c15a76b394efbe2721bf1162341d2bcfe744e5
Simon <simon.velasquezp@uqvirtual.edu.co>
2025-11-02 23:23:14 -0500

    Implement RangoCliente class with determinarRango method
    Added method to determine client range based on points.

--- proyecto final/src/app/model/RangoCliente.java
+++ proyecto final/src/app/model/RangoCliente.java
@@ -1,5 +1,16 @@
 package app.model;
 
 public class RangoCliente {
-    
+    public static String determinarRango(int puntos) {
+        if (puntos < 1000) {
+            return "Bronce";
+        } else if (puntos < 5000) {
+            return "Plata";
+        } else if (puntos < 10000) {
+            return "Oro";
+        } else {
+            return "Platino";
+        }
+    }
 }
+
