95634bba3785b0d7c54322413105bbb74ff212ae
Simon <simon.velasquezp@uqvirtual.edu.co>
2025-11-02 23:35:56 -0500

    Enhance CuentaBase with id field and methods
    Added 'id' field and its getter/setter methods to CuentaBase.

--- proyecto final/src/app/model/CuentaBase.java
+++ proyecto final/src/app/model/CuentaBase.java
@@ -5,15 +5,26 @@ public abstract class CuentaBase {
     protected String numeroCuenta;
     protected double saldo;
     protected Usuario propietario;
+    protected String id;
 
     public CuentaBase(String numeroCuenta, Usuario propietario) {
         this.numeroCuenta = numeroCuenta;
         this.propietario = propietario;
         this.saldo = 0;
+        this.id=id;
     }
-
+    public void setSaldo(double saldo){
+        this.saldo=saldo;
+    }
     public double getSaldo() { 
         return saldo; }
+    
+    public String getId(){
+        return id;
+    }    
+    public void setId(String id){
+        this.id=id;
+    }
 
     public void depositar(double monto) {
         if (monto > 0) saldo += monto;
@@ -24,4 +35,5 @@ public abstract class CuentaBase {
     }
     public abstract void mostrarResumen();
 }
+
 
