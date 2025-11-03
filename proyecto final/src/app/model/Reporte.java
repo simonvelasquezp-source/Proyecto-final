1424766704f2cba17e7dc1038d2ded5a0329554c
Simon <simon.velasquezp@uqvirtual.edu.co>
2025-11-02 23:24:25 -0500

    Implement client report generation in Reporte class
    Added method to generate client report with details.

--- proyecto final/src/app/model/Reporte.java
+++ proyecto final/src/app/model/Reporte.java
@@ -1,5 +1,16 @@
 package app.model;
 
 public class Reporte {
-    
+    public void generarReporteCliente(Cliente cliente, int puntos, ArrayList<Transaccion> transacciones){
+        System.out.println("Reporte");
+        System.out.println("Cuenta a nombre de:"+ cliente.getNombre());
+        System.out.println("Cedula:"+cliente.getCedula());
+        System.out.println("Correo:"+ cliente.getCorreo());
+        System.out.println("Numero telefonico:"+cliente.getTelefono());
+        System.out.println("Puntos actuales"+puntos);
+        System.out.println("Rango: " + RangoCliente.determinarRango(puntos));
+        System.out.println("Número de transacciones: " + transacciones.size());
+        
+    }
 }
+
