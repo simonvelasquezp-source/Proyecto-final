e614552b846f2ed80f4dfacc3e007d0c06c3d9c8
Simon <simon.velasquezp@uqvirtual.edu.co>
2025-11-16 02:17:05 -0500

    Arreglar metodo de Transaccion

--- src/app/model/Transaccion.java
+++ src/app/model/Transaccion.java
@@ -64,54 +64,53 @@ public abstract class Transaccion {
                 "Descripcion:" + descripcion;
     }
 
-    public static void procesarTransaccionesProgramadas(ArrayList<Transaccion> lista, ArrayList<Cliente> clientes) {
-        for (int i = 0; i < lista.size() - 1; i++) {
-            for (int j = 0; j < lista.size() - i - 1; j++) {
-                if (lista.get(j).getFecha().isAfter(lista.get(j + 1).getFecha())) {
-                    Transaccion temp = lista.get(j);
-                    lista.set(j, lista.get(j + 1));
-                    lista.set(j + 1, temp);
-                }
-            }
-        }
+    public static void procesarTransaccionesProgramadas(
+            ArrayList<Transaccion> lista,
+            ArrayList<Cliente> clientes
+    ) {
 
-        System.out.println("\n Procesando transacciones programadas...");
+        // 1. Ordenar por fecha (mucho más limpio que bubble sort)
+        lista.sort((a, b) -> a.getFecha().compareTo(b.getFecha()));
+
+        System.out.println("\nProcesando transacciones programadas...");
 
         for (Transaccion t : lista) {
+
             if (t.getFecha().isEqual(LocalDate.now())) {
+
                 System.out.println("Ejecutando transacción programada: " + t.getDescripcion());
 
                 for (Cliente c : clientes) {
                     if (c.getNombre().equalsIgnoreCase(t.getCliente())) {
-                        Monedero monedero = c.getMonederos();
 
-                        if (monedero != null) {
-                            if (t instanceof Deposito dep) {
-                                monedero.depositar(dep.getMonto());
-                            } else if (t instanceof Retiro ret) {
-                                monedero.retirar(ret.getMonto());
-                            } else if (t instanceof Transferencia trans) {
-                                monedero.transferir(trans.getMonto(), trans.getDestino());
-                            }
+                        ArrayList<Monedero> monederos = c.getMonederos();
+
+                        if (monederos != null && !monederos.isEmpty()) {
+
+                            Monedero m = monederos.get(0);
+
+                            t.aplicarTransaccion(m);
+
 
                             String mensaje = switch (t.getClass().getSimpleName()) {
-                                case "Deposito" -> " Depósito automático de $" + t.getMonto() + " realizado con éxito.";
-                                case "Retiro" -> " Retiro automático de $" + t.getMonto() + " ejecutado correctamente.";
-                                case "Transferencia" -> " Transferencia automática de $" + t.getMonto() + " completada.";
-                                default -> "ℹ Transacción automática ejecutada.";
+                                case "Deposito" -> "Depósito automático de $" + t.getMonto() + " realizado con éxito.";
+                                case "Retiro" -> "Retiro automático de $" + t.getMonto() + " ejecutado correctamente.";
+                                case "Transferencia" -> "Transferencia automática de $" + t.getMonto() + " completada.";
+                                default -> "Transacción automática ejecutada.";
                             };
 
                             Notificador.enviarWhatsApp(c, mensaje);
                             Notificador.enviarCorreo(c, "Transacción Programada Ejecutada", mensaje);
 
-                            monedero.agregarTransaccion(t);
+                            m.agregarTransaccion(t);
                         }
                     }
                 }
             }
         }
-}
     }
+
+}
 
 
 
