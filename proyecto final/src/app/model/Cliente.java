2e3f3ed289c221a593c76f91c30ed4f32c0eddd2
Simon <simon.velasquezp@uqvirtual.edu.co>
2025-11-16 02:46:18 -0500

    Enhance Cliente class with wallet and points features
    Added properties for wallet management and points system.

--- proyecto final/src/app/model/Cliente.java
+++ proyecto final/src/app/model/Cliente.java
@@ -1,9 +1,59 @@
 package app.model;
-
+import java.util.ArrayList;
 public class Cliente extends Usuario{
-    public Cliente(String nombre, String cedula, String correo, String contraseña, String telefono, String tipoUsuario, Transaccion transacciones){
-        super(nombre, cedula, correo, contraseña, telefono, tipoUsuario, transacciones);
+    public Cliente(String nombre, String cedula, String correo, String contraseña, String telefono, String tipoUsuario, Transaccion transacciones, Monedero monederos,  int puntos, String rangoActual){
+        super(nombre, cedula, correo, contraseña, telefono, tipoUsuario);
+        this.monederos= new ArrayList<>();
+        this.puntos=puntos;
+        this.rangoActual=rangoActual;
     }
-    
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+    public String getRangoActual(){
+        return rangoActual;
+    }
+
+    public void setRangoActual(String rangoActual) {
+        this.rangoActual = rangoActual;
+    }
+
+    @Override
+    public String getNombre() {
+        return super.getNombre();
+    }
+
+    @Override
+    public String getCedula() {
+        return super.getCedula();
+    }
+    public Monedero getMonedero(String nombreMonedero) {
+        for (Monedero m : monederos) {
+            if (m.getNombre().equalsIgnoreCase(nombreMonedero)) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+
+    public ArrayList<Monedero> getMonederos() {
+        return monederos;
+    }
+
+    public void setMonederos(ArrayList<Monedero> monederos) {
+        this.monederos = monederos;
+    }
+    public void agregarPuntos(int puntosGanados) {
+        this.puntos += puntosGanados;
+    }
+    public void agregarMonedero(Monedero m) {
+        this.monederos.add(m);
+    }
 }
+
 
