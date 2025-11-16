bde2644ab7ce27fab4dffcbd6847449a278ef3b4
Simon <simon.velasquezp@uqvirtual.edu.co>
2025-11-16 02:58:54 -0500

    Implement point assignment logic in Puntos class
    Added method to assign points based on transaction type.

--- proyecto final/src/app/model/Puntos.java
+++ proyecto final/src/app/model/Puntos.java
@@ -22,5 +22,22 @@ public class Puntos {
     public void agregarPuntos(int cantidad){
         this.puntos+= cantidad;
     }
+    public static void asignarPuntos(Cliente cliente, Transaccion transaccion) {
+        double monto = transaccion.getMonto();
+        int puntosGanados = 0;
+
+        if (transaccion instanceof Deposito) {
+            puntosGanados = (int) ((monto / 1000) * 10);
+        } else if (transaccion instanceof Retiro) {
+            puntosGanados = (int) ((monto / 100) * 2);
+        } else if (transaccion instanceof Transferencia) {
+            puntosGanados = (int) ((monto / 100) * 3);
+        }
+
+        cliente.agregarPuntos(puntosGanados);
+
+        System.out.println( puntosGanados + " puntos asignados al cliente " + cliente.getNombre());
+    }
 }
+
 
